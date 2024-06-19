7bba69f5c50fa13f94cb33be319a3358583bb957
Viet Anh Dang <anhdvvhe171788@fpt.edu.vn>
2024-06-19 22:06:05 +0700

    make empty branch

--- web/js/videoLearn.js
+++
@@ -1,8 +0,0 @@
-function toggleBtn(idContent) {
-    const contentVideo = document.getElementById(idContent);
-    if (contentVideo.style.display === 'none') {
-        contentVideo.style.display = 'block';
-    } else {
-        contentVideo.style.display = 'none';
-    }
-}
