156caf944fb63805bbffdd434352019a0bd59416
ThaiHE1735 <thaibqhe173335@fpt.edu.vn>
2024-07-01 17:42:35 +0700

    Update new dashboard

--- src/java/AdminManagementController/DisplayAccountRateDashboard.java
+++ src/java/AdminManagementController/DisplayAccountRateDashboard.java
@@ -53,7 +53,7 @@ public class DisplayAccountRateDashboard extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("dashboard1.jsp").forward(request, response);
     } 
 
     /** 
